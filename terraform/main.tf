0af32d9de2d741fae5a1bf74dc6fbb8aa2f53d65
Sap0 <salvoleonardi98@outlook.it>
2025-01-27 18:20:24 +0100

    fixed acr name and sizing node aks

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_B2als_v2"
   }
 
   identity {
