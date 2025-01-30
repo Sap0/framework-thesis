9d9f453d16e8b05b9a863cbe6d62f9dc5ccdce62
Sap0 <salvoleonardi98@outlook.it>
2025-01-30 17:55:07 +0100

    updated acr sku to standard

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
+  sku                 = "Standard"
   admin_enabled       = true
   anonymous_pull_enabled = true
 }
