64379efed40b95b0cd3d6cf30e4da911853cb9a6
Sap0 <salvoleonardi98@outlook.it>
2025-01-30 17:30:58 +0100

    test 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     network_plugin    = "azure"
-    load_balancer_sku = "Standard"
+    load_balancer_sku = "standard"
   }
 
   identity {
