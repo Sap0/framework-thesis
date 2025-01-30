7e796f0f89824fdb44312b8f307fc623591c9606
Sap0 <salvoleonardi98@outlook.it>
2025-01-30 19:33:36 +0100

    aks identity

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,13 +39,13 @@ resource "azurerm_user_assigned_identity" "aks_identity" {
 resource "azurerm_role_assignment" "aks_network" {
   scope                = azurerm_resource_group.acr.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
 }
 
 resource "azurerm_role_assignment" "aks_acr" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
 }
 
 # AKS cluster creation
